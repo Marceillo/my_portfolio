7a55d37b19f333eb817367a66e3a849ea658c24a
marceillo <marceillo@hotmail.com>
2025-06-05 13:59:29 +0200

    Dir update views/urls/tempplates

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # Add your URL patterns here
+    path('', views.home, name='home'),
+    #path('', include('ortfolio.urls')),
 ]
